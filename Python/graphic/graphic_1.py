bbbdd802112c9899df5e419cddc251e78184c1ae
grandq33769 <grandq33769@gmail.com>
2017-04-06 22:20:54 +0800

    20170406

---
+++ Python/graphic/graphic_1.py
@@ -0,0 +1,20 @@
+'''
+Created on 2017年4月6日
+
+@author: LokHim
+'''
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(0, 360)
+y = np.sin(x * np.pi / 180.0)
+
+plt.plot(x, y)
+plt.xlim(-30, 390)
+plt.ylim(-1.5, 1.5)
+plt.xlabel("x-axis")
+plt.ylabel("y-axis")
+plt.title("The Title")
+
+plt.show()
