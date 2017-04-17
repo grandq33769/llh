9188b5d6a9ee00ec3869c105c8f2ff379ced2e5b
grandq33769 <grandq33769@gmail.com>
2017-04-17 11:49:10 +0800

    20170417

--- Python/graphic/lift.py
+++ Python/graphic/lift.py
@@ -12,16 +12,17 @@ from matplotlib.ticker import LinearLocator, FormatStrFormatter
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 
-confidence = np.arange(0.1, 1,0.1)
-probability_of_left = np.arange(0.1, 1,0.1)
+confidence = np.arange(0.1, 1, 0.1)
+probability_of_left = np.arange(0.1, 1, 0.1)
 confidence, probability_of_left = np.meshgrid(confidence, probability_of_left)
 lift = confidence / probability_of_left
 
-surf = ax.plot_surface(confidence, probability_of_left, lift, cmap=cm.YlGnBu, linewidth=0, antialiased=False)
+surf = ax.plot_surface(confidence, probability_of_left,
+                       lift, cmap=cm.YlGnBu, linewidth=0, antialiased=False)
 ax.set_xlabel('Confidence')
 ax.set_ylabel('Probability of left')
 ax.set_zlabel('Lift')
-ax.set_zlim(0,10)
+ax.set_zlim(0, 10)
 
 plt.title('Change of Lift')
 fig.colorbar(surf, shrink=0.5, aspect=5)
