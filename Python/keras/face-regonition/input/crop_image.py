d75498a912c6842f359c8350aec47668f8376651
grandq33769 <grandq33769@gmail.com>
2017-05-10 22:44:17 +0800

    20170510

--- Python/keras/face-regonition/input/crop_image.py
+++ Python/keras/face-regonition/input/crop_image.py
@@ -8,16 +8,38 @@ from input_name import FILESET, URLBASE
 from input_ellipse import LOCATION_DICT
 import numpy as np
 
+PADDING_SIZE = 5
+
+def is_overlap(income, origin):
+    origin = origin + 5 
+
+def location_of_face(face):
+    start_x = face.xcoor - face.minor_axis
+    start_y = face.ycoor - face.major_axis
+    end_x = face.xcoor + face.minor_axis
+    end_y = face.ycoor + face.major_axis
+    return (start_x, start_y, end_x, end_y)
+
+def crop_negative(face_list, image, image_size):
+    location_list = []
+    width, height = image.size
+    
+    for face in face_list:
+        bbox = location_of_face(face)
+        location_list.append(bbox)
+        
+    for x_start in range(0,width,image_size):
+        for y_start in range(0,height,image_size):
+            rbox = (x_start,y_start,x_start+image_size,y_start+image_size)
+            if all([is_overlap(rbox,loc) for loc in location_list]):
+                
+
+    return return_list
 
 def crop_face(face_list, image):
     return_list = []
     for face in face_list:
-        start_x = face.xcoor - face.minor_axis
-        start_y = face.ycoor - face.major_axis
-        end_x = face.xcoor + face.minor_axis
-        end_y = face.ycoor + face.major_axis
-        bbox = (start_x, start_y, end_x, end_y)
-
+        bbox = location_of_face(face)
         new = image.rotate(face.angle)
         crop_image = new.crop(bbox)
         return_list.append(crop_image)
@@ -25,6 +47,7 @@ def crop_face(face_list, image):
     return return_list
 
 total = 0 #5171
+#Crop face image
 for filename in FILESET:
     with Image.open(URLBASE + '/' + filename + '.jpg', 'r') as image:
         face_list = LOCATION_DICT[filename]
@@ -34,7 +57,22 @@ for filename in FILESET:
         for croped_face in croped_list:
             index = croped_list.index(croped_face)
             str_index = str(index)
-            total += 1
             croped_face.save(save_path + '_' + str_index + '.jpg')
+            total += 1
+            
+print(total)
+
+#Crop background image
+total = 0
+for filename in FILESET:
+    with Image.open(URLBASE + '/' + filename + '.jpg', 'r') as image:
+        face_list = LOCATION_DICT[filename]
+        croped_list = crop_negative(face_list, image)
+        file_name = filename.replace('/','_')
+        save_path = URLBASE + '/Input_Data/Negative/' 
+        for croped_neg in croped_list:
+            str_index = str(total)
+            croped_neg.save(save_path + '_' + str_index + '.jpg')
+            total += 1
             
 print(total)
