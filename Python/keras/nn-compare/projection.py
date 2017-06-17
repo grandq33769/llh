259599e7f8c109c73b838161916b73868abccac3
lhleung <lhleung@lhleungde-MacBook-Pro.local>
2017-06-17 23:10:01 +0800

    20170615

--- Python/keras/nn-compare/projection.py
+++ Python/keras/nn-compare/projection.py
@@ -9,15 +9,16 @@ import random as r
 import input
 import plot
 
-di = input.DataInputter('D:/Ecllipse/my-code/Python/keras/nn-compare/mnist-data')
+di = input.DataInputter(
+    '/Users/lhleung/Documents/Code/llh/Python/keras/nn-compare/mnist-data/')
 images, labels = di.get_testing_data_and_labels()
 ran_list = []
 images_list = []
 num_of_pt = 1000
 
 print('Randoming...')
-for num in range(0,num_of_pt):
-    ran = r.randint(0,len(images)-1)
+for num in range(0, num_of_pt):
+    ran = r.randint(0, len(images) - 1)
     ran_list.append(ran)
     images_list.append(images[ran])
 
@@ -26,4 +27,4 @@ pca = PCA(n_components=2)
 transform = pca.fit(images_list).transform(images_list)
 
 print('Ploting...')
-plot.plot(transform,labels,ran_list)
+plot.plot(transform, labels, ran_list)
--- Python/keras/nn-compare/test.py
+++ Python/keras/nn-compare/test.py
@@ -5,7 +5,7 @@ Created on 2017年4月27日
 '''
 
 import keras
-import Plot
+import plot
 import random as r
 from keras.datasets import mnist
 from keras.models import load_model
@@ -19,11 +19,12 @@ x_train = x_train.astype('float32')
 x_train /= 255
 print(x_train.shape[0], 'train samples')
 
-model = load_model('DNN_2.h5')
+model = load_model(
+    '/Users/lhleung/Documents/Code/llh/Python/keras/nn-compare/DNN_2.h5')
 
 intermediate_layer_model = Model(inputs=model.input,
-                                 outputs=model.get_layer(index = 4).output)
-sample = x_train[0].reshape(1,784)
+                                 outputs=model.get_layer(index=4).output)
+sample = x_train[0].reshape(1, 784)
 intermediate_output = intermediate_layer_model.predict(x_train)
 
 pca = PCA(n_components=2)
@@ -32,10 +33,10 @@ transfrom = pca.fit(intermediate_output).transform(intermediate_output)
 ran_list = []
 images_list = []
 num_of_pt = 5000
-for num in range(0,num_of_pt):
-    ran = r.randint(0,len(transfrom)-1)
+for num in range(0, num_of_pt):
+    ran = r.randint(0, len(transfrom) - 1)
     ran_list.append(ran)
     images_list.append(transfrom[ran])
-    
+
 print(images_list[0])
-Plot.plot(images_list,y_train,ran_list)
+plot.plot(images_list, y_train, ran_list)
