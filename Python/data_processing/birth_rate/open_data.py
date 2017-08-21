bacd41239a3bfcd4e5030afe4f04c43962909191
lhleung <lhleung@lhleungde-MacBook-Pro.local>
2017-08-21 17:10:22 +0800

    Change open() Argument

--- Python/data_processing/birth_rate/open_data.py
+++ Python/data_processing/birth_rate/open_data.py
@@ -7,6 +7,6 @@ BASE_URL = os.path.dirname(__file__)
 print(BASE_URL)
 PATH = BASE_URL + '/Birth_Rate.csv'
 
-with open(PATH, encoding='utf-8', newline='\n') as csvfile:
+with open(PATH, encoding='utf-8') as csvfile:
     RAW = csvfile.read()
     print(RAW)
