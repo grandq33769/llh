703d560c6998345e33b15de7690b50f12e3ac77a
lhleung <lhleung@lhleungde-MacBook-Pro.local>
2017-09-28 17:19:49 +0800

    More test & print

--- Python/rl/cartpole_v0.py
+++ Python/rl/cartpole_v0.py
@@ -2,16 +2,31 @@
 Pratice program for reinforcement learning (CartPole-v0)
 Date : 2017/09/26
 '''
+import numpy as np
 import gym
 
+SHOW = True
+
 if __name__ == '__main__':
+    # print(gym.envs.registry.all())
     ENV = gym.make('CartPole-v0')
+    # print(dir(ENV.action_space))
     for episode in range(20):
         observation = ENV.reset()
-        for t in range(100):
-            print(observation)
+        # Upper bound & Lower bound of observation space
+        # print(ENV.observation_space.high)
+        # print(ENV.observation_space.low)
+        # print(dir(ENV.observation_space))
+        pre_obs = np.zeros(ENV.observation_space.shape)
+        for t in range(1000):
+            if SHOW:
+                ENV.render()
+            # print(observation)
+            # print(dir(observation))
             action = ENV.action_space.sample()
             observation, reward, done, info = ENV.step(action)
+            pre_obs = observation
             if done:
-                print("Episode finished after {} timesteps".format(t + 1))
+                print("Episode {} finished after {} timesteps".format(
+                    episode + 1, t + 1))
                 break
