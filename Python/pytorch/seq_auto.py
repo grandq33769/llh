3cf454cecd2d4453e8a21d5b1c25f34b9a4aa87d
lhleung <lhleung@lhleungde-MacBook-Pro.local>
2017-09-29 23:02:56 +0800

    Finish optimization part

--- Python/pytorch/seq_auto.py
+++ Python/pytorch/seq_auto.py
@@ -4,12 +4,14 @@ Date : 2017/9/29
 '''
 import torch
 from torch import nn
+from torch.autograd import Variable
 
 torch.manual_seed(1)
 
 # Hyper Parameters
+EPOCH = 1
 INPUT_SIZE = 400      # rnn input size
-LR = 0.005           # learning rate
+LR = 0.003           # learning rate
 # TODO: Get Total Vocabulary Size (int)
 VOCAB_SIZE = 100
 
@@ -43,10 +45,10 @@ class RNNautoencoder(nn.Module):
             bidirectional=self.bidirectional
         )
         # Seems like unnecessary. If delete this line, correct forward()
-        self.e2d = nn.Linear(self.hidden_size, self.input_size)
+        self.e2d = nn.Linear(self.hidden_size, self.hidden_size)
 
         self.decoder = nn.GRU(
-            input_size=self.input_size,
+            input_size=self.hidden_size,
             hidden_size=self.hidden_size,
             num_layers=self.layers,
             batch_first=True,
@@ -65,6 +67,7 @@ class RNNautoencoder(nn.Module):
         '''
         # x (batch, time_step, input_size)
         # eh_state (n_layers, batch, hidden_size)
+        # dh_state (n_layers, batch, hidden_size)
         # e_out (batch, time_step, hidden_size)
         e_out, eh_state = self.encoder(x, eh_state)
         # Only last time step to be input
@@ -73,4 +76,35 @@ class RNNautoencoder(nn.Module):
         d_out = d_out.view(-1, self.hidden_size)
         output = self.out(d_out)
 
+        '''
+        output (batch, sentense_len, hidden_size)
+        d_input (hidden_size)
+        '''
         return output, d_input, eh_state, dh_state
+
+
+if __name__ == '__main__':
+    rnn_auto = RNNautoencoder(VOCAB_SIZE)
+    print(rnn_auto)
+
+    optimizer = torch.optim.Adam(rnn_auto.parameters(), lr=LR)
+    loss_func = nn.CrossEntropyLoss()
+
+    e_state = None
+    d_state = None
+    for epoch in range(EPOCH):
+        for step, sentense in enumerate(train_loader):
+            b_x = Variable(sentense.view(-1, INPUT_SIZE))
+            b_y = Variable(sentense.view(-1, VOCAB_SIZE))
+
+            # emb is our final target
+            out, emb, e_state, dstate = rnn_auto(b_x, e_state, d_state)
+            e_state = Variable(e_state.data)
+            d_state = Variable(d_state.data)
+
+            loss = loss_func(out, b_y)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            print('Steps: {:d} | Training Loss: {:4f}'.format(
+                step, loss.data[0]))
