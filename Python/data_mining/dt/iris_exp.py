94aed9be74318890a59b451690d4ab21ea880050
lhleung <grandq33769@gmail.com>
2017-12-01 21:13:20 +0800

    Initialization

--- Python/data_mining/dt/iris_exp.py
+++ Python/data_mining/dt/iris_exp.py
@@ -1,6 +1,12 @@
+'''
+Script for building decision tree
+Date: 2017/12/1
+'''
+
 from sklearn.datasets import load_iris
 
-data = load_iris()
+DATA = load_iris()
 
 #Attribute: ['DESCR', 'data', 'feature_names', 'target', 'target_names']
-print(data.target)
+print(DATA.target)
+print(type(DATA))
